e43614a24f66b0a98275c0c527c290e8796d35a8
Lakshay <lakshaybankura393@gmail.com>
2025-10-22 11:08:28 +0530

    commit on  clone repo

--- server/server.js
+++ server/server.js
@@ -23,6 +23,6 @@ app.use("/api/users", userRouter);
 // Connect to DB
 connectDB();
 
-// Start Server
+//To Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
